104ab4e493948bc23582e4917258650778c9f2cc
David Hooker <dhooker@ganart.net>
2011-04-26 14:19:14 -0500

    Initial code commit

---
+++ src/main/java/net/dudehook/cliche2/InputConverter.java
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the Cliche project, licensed under MIT License. See LICENSE.txt file in root folder of Cliche
+ * sources.
+ */
+
+package net.dudehook.cliche2;
+
+/**
+ * This interface is used by the Shell to support new argument types. It converts string to an object of given class.
+ * 
+ * @author ASG
+ */
+public interface InputConverter
+{
+    /**
+     * String-to-someClass conversion method May throw any exception if string is considered invalid for given class;
+     * must do nothing but return null if doesn't recognize the toClass.
+     * 
+     * @param original
+     *            String to be converted
+     * @param toClass
+     *            Class to be converted to
+     * @return Object of the class toClass or <strong>null</strong>, if don't know how to convert to given class
+     * 
+     * @see asg.cliche.Shell
+     */
+    Object convertInput(String original, Class toClass) throws Exception;
+}
